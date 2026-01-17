1bea248404a80dcf9b0d3a414273d0f0b3cf7a04
meharparnami14 <meharparnami14@gmail.com>
2026-01-17 22:57:11 +0530

    Create SubscriptionController.java

---
+++ backend/src/main/java/com/recurly/SubscriptionController.java
@@ -0,0 +1,29 @@
+package com.recurly;
+
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@CrossOrigin(origins = "*")
+@RequestMapping("/subscriptions")
+public class SubscriptionController {
+
+    private final SubscriptionRepository repository;
+
+    public SubscriptionController(SubscriptionRepository repository) {
+        this.repository = repository;
+    }
+
+    // Dashboard
+    @GetMapping
+    public List<Subscription> getAllSubscriptions() {
+        return repository.findAll();
+    }
+
+    // Add Subscription
+    @PostMapping
+    public Subscription addSubscription(@RequestBody Subscription subscription) {
+        return repository.save(subscription);
+    }
+}
